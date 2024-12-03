b02cf18c1f42b1ded64ea9076603efa237b27106
mweng112 <maggie.weng112@gmail.com>
2024-12-03 12:20:46 -0800

    Buffer Error Resolved

--- src/mqtt-client.ts
+++ src/mqtt-client.ts
@@ -1,31 +1,35 @@
-import * as mqtt from "mqtt"
-import { IClientOptions, IClientPublishOptions, MqttClient as MQTTClientType } from "mqtt"
+import mqtt from "mqtt";
+import {
+  IClientOptions,
+  IClientPublishOptions,
+  MqttClient as MQTTClientType,
+} from "mqtt";
 
-const MQTT_TIMEOUT_SECONDS = 3
-const MQTT_TIMEOUT_MS = MQTT_TIMEOUT_SECONDS * 1000
+const MQTT_TIMEOUT_SECONDS = 3;
+const MQTT_TIMEOUT_MS = MQTT_TIMEOUT_SECONDS * 1000;
 
-const KEEP_ALIVE_SECONDS = 5
-const RECONNECT_INTERVAL_MS = 1 * 1000 // 1 second
+const KEEP_ALIVE_SECONDS = 5;
+const RECONNECT_INTERVAL_MS = 1 * 1000;
 
 function getClientId(): string {
-  return "pendant_" + Math.random().toString(16).substr(2, 8)
+  return "pendant_" + Math.random().toString(16).substr(2, 8);
 }
 
 export interface Callback {
-  (topic: string, message: string): void
+  (topic: string, message: string): void;
 }
 
 interface Subscription {
-  pattern: RegExp
-  callback: Callback
+  pattern: RegExp;
+  callback: Callback;
 }
 
 class MqttClient {
-  private mqttClient?: MQTTClientType
-  private subscriptions: Subscription[] = []
+  private mqttClient?: MQTTClientType;
+  private subscriptions: Subscription[] = [];
 
   constructor(url: string, connectCallback?: () => void) {
-    this.connect(url, connectCallback)
+    this.connect(url, connectCallback);
   }
 
   connect(url: string, connectCallback?: () => void): void {
@@ -34,58 +38,67 @@ class MqttClient {
       clientId: getClientId(),
       reconnectPeriod: 0,
       keepalive: KEEP_ALIVE_SECONDS,
-    }
+    };
 
-    this.mqttClient = mqtt.connect(url, options)
+    this.mqttClient = mqtt.connect(url, options);
 
     this.mqttClient.on("connect", () => {
-      connectCallback?.()
-    })
+      connectCallback?.();
+    });
 
     this.mqttClient.on("close", () => {
-      console.warn("MQTT connection closed")
+      console.warn("MQTT connection closed");
       this.mqttClient?.end(true, {}, () => {
-        console.log("MQTT connection ended")
-        this.mqttClient = undefined
-        setTimeout(() => this.connect(url, connectCallback), RECONNECT_INTERVAL_MS)
-      })
-    })
+        console.log("MQTT connection ended");
+        this.mqttClient = undefined;
+        setTimeout(
+          () => this.connect(url, connectCallback),
+          RECONNECT_INTERVAL_MS
+        );
+      });
+    });
 
     this.mqttClient.on("error", (error: Error) => {
-      console.error("Error encountered in mqtt client: ", error)
-    })
+      console.error("Error encountered in mqtt client: ", error);
+    });
 
     this.mqttClient.on("message", (topic: string, message: Buffer) => {
-      this.subscriptions.forEach(sub => {
+      this.subscriptions.forEach((sub) => {
         if (sub.pattern.test(topic)) {
-          sub.callback(topic, message.toString())
+          sub.callback(topic, message.toString());
         }
-      })
-    })
+      });
+    });
   }
 
   subscribe(topic: string, regex: RegExp, cb: Callback): void {
-    this.subscriptions.push({ pattern: regex, callback: cb })
+    this.subscriptions.push({ pattern: regex, callback: cb });
     this.mqttClient?.subscribe(topic, (err: Error) => {
       if (err) {
-        this.subscriptions = this.subscriptions.filter(sub => sub.pattern !== regex || sub.callback !== cb)
-        console.error("Unable to subscribe to MQTT broker with error: ", err)
+        this.subscriptions = this.subscriptions.filter(
+          (sub) => sub.pattern !== regex || sub.callback !== cb
+        );
+        console.error("Unable to subscribe to MQTT broker with error: ", err);
       }
-    })
+    });
   }
 
   unsubscribe(topic: string, regex: RegExp, cb: Callback): void {
-    this.subscriptions = this.subscriptions.filter(sub => sub.pattern !== regex || sub.callback !== cb)
-    const sameTopicSubscriptions = this.subscriptions.filter(sub => String(sub.pattern) === String(regex))
+    this.subscriptions = this.subscriptions.filter(
+      (sub) => sub.pattern !== regex || sub.callback !== cb
+    );
+    const sameTopicSubscriptions = this.subscriptions.filter(
+      (sub) => String(sub.pattern) === String(regex)
+    );
     if (sameTopicSubscriptions.length <= 0) {
-      this.mqttClient?.unsubscribe(topic)
+      this.mqttClient?.unsubscribe(topic);
     }
   }
 
   publish(topic: string, message: string, retain = false): void {
-    const options: IClientPublishOptions = { retain }
-    this.mqttClient?.publish(topic, message, options)
+    const options: IClientPublishOptions = { retain };
+    this.mqttClient?.publish(topic, message, options);
   }
 }
 
-export { MqttClient }
+export { MqttClient };
