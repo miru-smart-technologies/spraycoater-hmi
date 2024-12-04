c2cdca93057b4c8c824ba9a3a838d19a5eba44ee
EliCC-Miru <e.carlincoleman@mirucorp.com>
2024-12-04 11:44:46 +0200

    MQTT working!

--- src/mqtt-lib.js
+++ src/mqtt-lib.js
@@ -1,24 +1,42 @@
-import { MqttClient } from './mqtt-client.ts';
+import mqtt, { MqttClient } from "mqtt"
 
 const url = 'mqtt://192.168.7.2:1883';
 
-const client = new MqttClient(url, () => {
-  client.subscribe('devices/io-expander/+/digital-input/Run', /Run/, (topic, message) => {
-    console.log('Received message on Run:', message);
-  });
+// Connect to the MQTT broker
+const client = mqtt.connect(url);
 
-  client.subscribe('Ready', /Ready/, (topic, message) => {
-    console.log('Received message on Ready:', message);
-  });
-
-  client.subscribe('Finished', /Finished/, (topic, message) => {
-    console.log('Received message on Finished:', message);
+// Subscribe to a topic
+client.on('connect', () => {
+  console.log('Connected to MQTT broker');
+  client.subscribe('Run', (err) => {
+    if (!err) {
+      console.log('Subscribed to Run');
+    } else {
+      console.error('Failed to subscribe:', err);
+    }
   });
 });
 
+// Handle incoming messages
 client.on('message', (topic, message) => {
-    console.log('Received message:', message.toString());
-  }
-);
+  console.log(`Received message on ${topic}: ${message.toString()}`);
+});
+
+// Publish a message to a topic
+client.on('connect', () => {
+  client.publish('Ready', "True", (err) => {
+    if (!err) {
+      console.log('Message published');
+    } else {
+      console.error('Failed to publish message:', err);
+    }
+  });
+});
+
+// Handle errors
+client.on('error', (err) => {
+  console.error('MQTT error:', err);
+});
+
 
 export default client;
