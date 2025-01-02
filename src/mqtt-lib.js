88021068ec33c1a350c0f576021a6cfab657c3a7
EliCC-Miru <e.carlincoleman@mirucorp.com>
2025-01-02 13:39:08 -0800

    Send test publish

--- src/mqtt-lib.js
+++ src/mqtt-lib.js
@@ -1,11 +1,14 @@
 import { MqttClient } from './mqtt-client.ts';
 
-const url = 'mqtt://192.168.7.2:1883';
+// const url = 'mqtt://192.168.7.2:1883';
+const url = 'mqtt://localhost:1884';
 
 const client = new MqttClient(url);
 
 client.subscribe('*', RegExp(".*"), (topic, message) => {
     console.log(`Received message on topic ${topic}: ${message}`);
 })
+console.log("Subscribed to all topics");
+client.publish('websocket', 'Hello from the client!');
 
 export default client;
