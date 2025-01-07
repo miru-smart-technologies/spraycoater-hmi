14e3758ba7459db86dcc8e3d17cebdc6f63ee85f
mweng112 <maggie.weng112@gmail.com>
2025-01-07 12:41:26 -0800

    Added icons

--- src/components/DeveloperWindow.js
+++ src/components/DeveloperWindow.js
@@ -66,7 +66,21 @@ function DeveloperWindow() {
             className="close-window-button"
             onClick={onCloseWindowButtonClick}
           >
-            x
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-x"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
           </button>
         </div>
         <div className="log-container" ref={logContainerRef}>
@@ -78,7 +92,22 @@ function DeveloperWindow() {
           className="open-window-button button"
           onClick={onOpenWindowButtonClick}
         >
-          Dev Logs
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-terminal"
+          >
+            <polyline points="4 17 10 11 4 5" />
+            <line x1="12" x2="20" y1="19" y2="19" />
+          </svg>
+          <span>Dev Logs</span>
         </div>
       </div>
     </div>
